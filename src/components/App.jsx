a19c3a467451f0b78665c4fbc63a72457db75aa5
Allen Hsieh <allenhsiehhr@gmail.com>
2018-08-11 13:23:35 -0700

    Passing VideoList and VideoListEntry tests!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,55 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><h5><em>videoPlayer</em> view goes here</h5></div>
+          </div>
+          <div className="col-md-5">
+            <div><h5><em>videoList</em> view goes here</h5></div>
+            {/* <VideoList videos="videos"/> */}
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
+
+// class GroceryListItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       hover: false
+//     }
+//   }
+
+//   onHover() {
+//     this.setState({
+//       hover: !this.state.hover
+//     })
+//   }
+
+//   render() {
+//     var style = {
+//       fontWeight: this.state.hover ? 'bold' : 'normal'
+//     }
+
+//     return (
+//       <li style = {style} onMouseEnter={this.onHover.bind(this)} onMouseLeave={this.onHover.bind(this)}>{this.props.list}</li>
+//     )
+//   }
+// }
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,21 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {/* <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
+    {props.videos.map(video =>
+      <VideoListEntry video={video} />
+    )}
+
   </div>
 );
+
+
+// var GroceryList = (props) => (
+//   <ul>
+//     {props.list.map(list =>
+//       <GroceryListItem list={list} />
+//     )}
+//   </ul>
+// );
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
